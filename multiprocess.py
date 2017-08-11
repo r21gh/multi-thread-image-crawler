1a2b2f576a14d579c6ed7834454eda8847e2fbf0
Reza Ghanbari <rezapython@gmail.com>
2017-08-11 17:09:18 +0430

    Multi processing model

---
+++ multiprocess.py
@@ -0,0 +1,18 @@
+from functools import partial
+from multiprocessing.pool import Pool
+from download import *
+from time import time
+
+
+def main():
+    ts = time()
+    client_id = 'a5921180d835431'
+    if not client_id:
+        raise Exception("Couldn't find IMGUR_CLIENT_ID environment variable!")
+    download_dir = setup_download_dir()
+    links = [l for l in get_links(client_id) if l.endswith('.jpg')]
+
+    download = partial(download_link, download_dir)
+    with Pool(8) as p:
+        p.map(download, links)
+    print('Took {}s'.format(time() - ts))
